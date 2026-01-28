a0b0109f27eb675e828673eaa0b106aa10d7a83c
RAMSEYC4 <ramseycedou68@gmail.com>
2026-01-28 15:30:59 +0200

    week 3 part2 work

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const inventoryRoute = require("./routes/inventoryRoute")
 const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
   View Engine and Templates
@@ -19,6 +21,22 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 
 /* ***********************
  * Local Server Information
@@ -35,7 +53,9 @@ app.listen(port, () => {
 })
 
 //index route
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" })
-})
+// app.get("/", function (req, res) {
+//   res.render("index", { title: "Home" })
+// })
+app.get("/", baseController.buildHome);
+app.use("/inv", inventoryRoute);
 
